099369f442e9518711a387e56fb3c93107059253
Marcos Santiago Rivera (CTS-SI) <msantiago@ocpr.gov.pr>
2023-04-24 15:15:03 -0400

    Made the group page more responsive

--- public/js/showGroup.js
+++ public/js/showGroup.js
@@ -1,16 +1,28 @@
 const opciones = document.querySelectorAll("#octOp");
+const actividadesYGalerias = document.querySelector("#actYGal");
+const titleImageG = document.querySelector("#titleImage");
+const opGroup = document.querySelectorAll(".octOp");
+const mediaQuery = window.matchMedia("(max-width: 1042px)");
+const mediaQuery2 = window.matchMedia("(max-min: 790px)");
 
-//do an event listener for each option in the array when clicked
+actividadesYGalerias.style.width = "1000px";
+
 for (let opcion of opciones) {
-  //do a function when clicked
   opcion.addEventListener("click", function (e) {
     e.preventDefault();
+
     for (let opcion of opciones) {
       opcion.parentNode.classList.remove("selected");
     }
 
-    //add selected to the parent of the clicked option
-
     opcion.parentNode.classList.add("selected");
+
+    console.log(opcion.childNodes[0].textContent);
   });
 }
+
+//set an event listener for activities and gallery whenever the screen is resized
+window.addEventListener("resize", function () {
+  titleImageG.style.height = titleImageG.clientWidth * 0.35 + "px";
+  actividadesYGalerias.style.width = "100%";
+});
